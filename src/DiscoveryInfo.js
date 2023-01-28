f96e036ff66e6cac27e727169e7a0777eb599da5
eyezek <71928231+eyezek@users.noreply.github.com>
2023-01-28 13:45:14 -0500

    added songs/artists/albums

--- src/DiscoveryInfo.js
+++ src/DiscoveryInfo.js
@@ -4,9 +4,16 @@ class DiscoveryInfo extends React.Component {
   render() {
     return (
       <div id="discoveryinfocontainer">
+        <h2 id="discoverytype">{this.props.type}</h2>
         <img className="avatar" alt="Avatar" src={this.props.img} />
-        <h2 className="discoverytitle">{this.props.artist}</h2>
+        <h2 className="discoverytitle">{this.props.title}</h2>
         <h3 className="description">{this.props.description}</h3>
+        <a className="links" target="_blank" href={this.props.youtubelink}>
+          <h3>YouTube</h3>
+        </a>
+        <a className="links" target="_blank" href={this.props.spotifylink}>
+          <h3>Spotify</h3>
+        </a>
       </div>
     );
   }
--- src/DiscoveryModal.js
+++ src/DiscoveryModal.js
@@ -8,32 +8,59 @@ class DiscoveryModal extends React.Component {
     return (
       <div>
         <Carousel
-          infiniteLoop={true}
           showThumbs={false}
           showStatus={false}
-          centerMode={true}
-          centerSlidePercentage={100}
+          showIndicators={false}
           width={650}
         >
           <div id="discoverymodal">
             <DiscoveryInfo
-              img={require("./images/placeholder.jpg")}
-              artist="Artist"
-              description="Description"
+              type="Song"
+              img={require("./images/covers/NERD-InSearchOfUK.jpg")}
+              title="N.E.R.D - Things Are Getting Better"
+              // description="From the album In Search Of"
+              youtubelink="https://www.youtube.com/watch?v=Ye2dXJXGMtA"
+              spotifylink="https://open.spotify.com/track/3rTboU1EiIkKlA4CQh5i8W?si=59f82183e96e4655"
             />
           </div>
           <div id="discoverymodal">
             <DiscoveryInfo
-              img={require("./images/placeholder.jpg")}
-              artist="Artist"
-              description="Description"
+              type="Artist"
+              img={require("./images/covers/knxwledge.jpg")}
+              title="Knxwledge"
+              // description="Description"
+              youtubelink="https://www.youtube.com/@knxwledge"
+              spotifylink="https://open.spotify.com/artist/17Zu03OgBVxgLxWmRUyNOJ?si=c83bb0f8f4b74da7"
             />
           </div>
           <div id="discoverymodal">
             <DiscoveryInfo
-              img={require("./images/placeholder.jpg")}
-              artist="Artist"
-              description="Description"
+              type="Song"
+              img={require("./images/covers/LTD-LoveToTheWorld.jpg")}
+              title="L.T.D. - Love Ballad"
+              // description="Description"
+              youtubelink="https://www.youtube.com/watch?v=rZHgUQqiWDI"
+              spotifylink="https://open.spotify.com/track/3iCbTmcXnKlRZCdaYOukxq?si=f16e1b7d30c94966"
+            />
+          </div>
+          <div id="discoverymodal">
+            <DiscoveryInfo
+              type="Album"
+              img={require("./images/covers/Mustafa-WhenSmokeRises.jpg")}
+              title="Mustafa - When Smoke Rises"
+              // description="Description"
+              youtubelink="https://www.youtube.com/watch?v=7E26_sjxbYY&list=PLoKyeCy41pGzSi_FMeRSze975m-HUWbQ6"
+              spotifylink="https://open.spotify.com/album/0iPWPiOsdGaWgcy9RMpvXw?si=dNdxvIqtSbW10UF9L0212g"
+            />
+          </div>
+          <div id="discoverymodal">
+            <DiscoveryInfo
+              type="Song"
+              img={require("./images/covers/foreign-exchange-liab.jpg")}
+              title="The Foreign Exchange - Leave It All Behind"
+              // description="Description"
+              youtubelink="https://www.youtube.com/watch?v=btS0v21HjiI"
+              spotifylink="https://open.spotify.com/track/5M3czI8WoA43GbldxswMUY?si=def84f3146bf4ede"
             />
           </div>
         </Carousel>
