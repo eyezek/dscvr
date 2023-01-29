1f9a2da9ad9122b4500bbe0a32646cfe1e93260b
eyezek <71928231+eyezek@users.noreply.github.com>
2023-01-29 14:56:05 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./Main";
+import Intro from "./Intro";
 import Discovery from "./Discovery";
 
 function App() {
   return (
     <div className="App">
-      {/* <Main /> */}
-      <Discovery />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/app" element={<Discovery />} />
+          {/* <Main /> */}
+          {/* <Discovery /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/SignInCreateAccount.js
+++ src/SignInCreateAccount.js
@@ -1,25 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-class SignInCreateAccount extends React.Component {
-  render() {
-    return (
-      <div id="logindiv">
-        <div id="signincreateaccountcontainer">
-          <button className="signinbuttons" id="createaccount">
-            Create Account
-          </button>
-          <button className="signinbuttons" id="signin">
-            Sign In
-          </button>
-        </div>
-        <div id="continueasguestcontainer">
-          <button className="signinbuttons" id="continueasguest">
-            Continue As Guest
-          </button>
-        </div>
+const SignInCreateAccount = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div id="logindiv">
+      <div id="signincreateaccountcontainer">
+        <button className="signinbuttons" id="createaccount">
+          Create Account
+        </button>
+        <button className="signinbuttons" id="signin">
+          Sign In
+        </button>
       </div>
-    );
-  }
-}
+      <div id="continueasguestcontainer">
+        <button
+          onClick={() => navigate("/app")}
+          className="signinbuttons"
+          id="continueasguest"
+        >
+          Continue As Guest
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default SignInCreateAccount;
